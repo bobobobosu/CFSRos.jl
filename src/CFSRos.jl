5d7b55059313e0dcb447f5add2efa21bfc16d90a
bobobobosu <bobobobosu@gmail.com>
2025-04-13 19:01:21 -0700

    disable precompile

--- src/CFSRos.jl
+++ src/CFSRos.jl
@@ -9,7 +9,7 @@ using PrecompileTools: @setup_workload, @compile_workload
 
 @setup_workload begin
     @compile_workload begin
-        include("precompile_workload.jl")
+        # include("precompile_workload.jl")
     end
 end
 
