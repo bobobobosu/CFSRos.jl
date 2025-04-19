a7aad4d4e3595d0531d59a3536d5871d7448feda
bobobobosu <bobobobosu@gmail.com>
2025-04-18 22:49:45 -0700

    tune trust region

--- src/cfsmotion.jl
+++ src/cfsmotion.jl
@@ -146,7 +146,7 @@ function solve_cfsmotion(
     begin
         objective_val, objective_val_new = nothing, nothing
         Δdg = 1.0e-2
-        Δdq = fill(1.0e-2, lastindex(trjy.cs))
+        Δdq = fill(1.0, lastindex(trjy.cs))
         for iter in 1:kinematics_iter
             m = Model(() -> Clarabel.Optimizer())
             set_optimizer_attribute(m, "tol_gap_abs", 1.0e-9)
